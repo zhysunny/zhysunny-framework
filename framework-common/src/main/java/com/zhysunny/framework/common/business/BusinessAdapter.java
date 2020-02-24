7040f0c3fab9e424766a353eb6cc7d384145fa59
zhangyun <0049003310@znv.com>
2020-02-24 16:45:38 +0800

    适配器

--- framework-common/src/main/java/com/zhysunny/framework/common/business/BusinessAdapter.java
+++ framework-common/src/main/java/com/zhysunny/framework/common/business/BusinessAdapter.java
@@ -1,6 +1,6 @@
 package com.zhysunny.framework.common.business;
 
-import java.util.Collection;
+import com.zhysunny.framework.common.exception.UnImplementedMethodException;
 import java.util.List;
 import java.util.Map;
 
@@ -12,22 +12,22 @@ public class BusinessAdapter<T> implements Business<T> {
 
     @Override
     public List<T> conversion(List<T> datas) {
-        return null;
+        throw new UnImplementedMethodException();
     }
 
     @Override
     public Map<String, T> conversionToMap(List<T> datas) {
-        return null;
+        throw new UnImplementedMethodException();
     }
 
     @Override
     public List<T> conversionToList(Map<String, T> datas) {
-        return null;
+        throw new UnImplementedMethodException();
     }
 
     @Override
     public Map<String, T> conversion(Map<String, T> datas) {
-        return null;
+        throw new UnImplementedMethodException();
     }
 
 }
